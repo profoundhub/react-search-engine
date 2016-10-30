d4a7987dc342122e7d6fb8040f6d2e5df5128bf5
Daniel Lim <hbt@markhamboards.com>
2016-10-30 19:34:51 -0400

    add GifList

--- react-search/src/index.js
+++ react-search/src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GifList from './components/GifList';
 import SearchBar from './components/SearchBar';
 
 const display = document.getElementById('app');
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [
+        {
+          id: 1,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 2,
+          url: 'http://fakeimg.pl/300/'
+        },
+        {
+          id: 3,
+          url: 'http://fakeimg.pl/300/'
+        }
+      ]
+    }
+  } 
+
   handleTermChange(term) {
     console.log(term);
   }
@@ -13,6 +36,7 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar onTermChange={ this.handleTermChange } />
+        <GifList gifs={this.state.gifs} />
       </div>
     );
   }
