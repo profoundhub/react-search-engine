2f8ec84b8378c59740de4073b2c2448d511ab2b5
Daniel Lim <hbt@markhamboards.com>
2016-10-30 17:50:17 -0400

    Win
    Win

--- react-search/src/components/SearchBar.js
+++ react-search/src/components/SearchBar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' }
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onTermChange(term);
+  }
+
   render() {
     return (
-      <div>
-        <h1>Search!</h1>
+      <div className="search">
+        <input onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
   }
