1b695bf914ad2baf2dbe8f152ca96490cdc1a691
Daniel Lim <hbt@markhamboards.com>
2016-10-30 17:45:02 -0400

    update Sets
    update Sets

--- react-search/src/index.js
+++ react-search/src/index.js
@@ -7,10 +7,23 @@ class App extends React.Component {
   render() {
     return (
       <div class="greeting">
-        <p class="greeting-text">Hello Happy World!</p>
+        <p class="greeting-text">Hello Happy Worlds!</p>
       </div>
     );
   }
 }
 
+/* Was:
+
+return React.createElement(
+        "div",
+        { "class": "greeting" },
+        React.createElement(
+              "p",
+              { "class": "greeting-text" },
+              "Hello Happy World!"
+        )
+);
+
+*/
 ReactDOM.render(<App />, display);
