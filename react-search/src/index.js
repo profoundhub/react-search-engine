301804461e12d0e1a59a253cd5e982197d884ac6
Daniel Lim <hbt@markhamboards.com>
2016-10-30 19:54:30 -0400

    dbl chk

--- react-search/src/index.js
+++ react-search/src/index.js
@@ -18,13 +18,10 @@ class App extends React.Component {
 
   handleTermChange(term) {
     console.log(term);
-    
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ term }&api_key=dc6zaTOxFJmzC`;
-
-    request.get(url, function(err, res) {
-      console.log(res.body.data[0]);
-    });
-
+    const url = `http://api.giphy.com/v1/gifs/search?q=${ term.replace(/\s/g, '+') }&api_key=dc6zaTOxFJmzC`;
+      request.get(url, function(err, res) {
+        console.log(res.body.data[0]);
+      });
   }
 
   render() {
