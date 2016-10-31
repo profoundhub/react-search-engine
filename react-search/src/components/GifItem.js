ec379a3fbb70c6bd4cbd04f0a302c9f23a1bf46f
Daniel Lim <hbt@markhamboards.com>
2016-10-30 20:40:40 -0400

    GifItem.js:25 Uncaught TypeErrors

--- react-search/src/components/GifItem.js
+++ react-search/src/components/GifItem.js
@@ -6,8 +6,8 @@ import React from 'react';
 
     const GifItem = ({ gif, onGifSelect }) => {
         return (
-            <div className="gif-item">
-                <img src={ image.gif.images.downsized.url } alt="" />        
+            <div className="gif-item" onClick={ () => onGifSelect(gif) }>
+                <img src={ gif.images.downsized.url } alt="" />        
             </div>
         )
     };
