5fb9b6c4fc078a9e8bda676df05d360196e73435
Daniel Lim <hbt@markhamboards.com>
2016-10-30 20:01:34 -0400

    scopes
    scopes

--- react-search/src/components/GifItem.js
+++ react-search/src/components/GifItem.js
@@ -5,9 +5,12 @@ import React from 'react';
 const GifItem = (image) => {
   return (
     <li>
-      <img src={ image.gif.url } alt="" />
+        <img src={image.gif.images.downsized.url} />
+        
     </li>
   )
 };
+
+// <img src={ image.gif.url } alt="" />
 
 export default GifItem;
