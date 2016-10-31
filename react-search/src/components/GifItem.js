101a66b817a2130297bf48ca775c6854ace21111
Daniel Lim <hbt@markhamboards.com>
2016-10-30 20:03:31 -0400

    ready - v1

--- react-search/src/components/GifItem.js
+++ react-search/src/components/GifItem.js
@@ -5,7 +5,7 @@ import React from 'react';
 const GifItem = (image) => {
   return (
     <li>
-        <img src={image.gif.images.downsized.url} />
+        <img src={image.gif.images.downsized.url} alt="" />
         
     </li>
   )
