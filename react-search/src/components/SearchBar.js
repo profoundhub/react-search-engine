a61b0957fb886b104923275905578c72571aa826
Daniel Lim <hbt@markhamboards.com>
2016-10-30 20:27:51 -0400

    modalIsOpen === True?

--- react-search/src/components/SearchBar.js
+++ react-search/src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  
+
   constructor(props) {
     super(props);
     this.state = { term: '' }
@@ -14,8 +14,14 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-      <div className="search">
-        <input onChange={ event => this.onInputChange(event.target.value) } />
+      <div>
+        <div className="ProfoundAround">
+          <img src="//profoundideation.com/videos/wp-content/uploads/2015/10/Profound-Ideation-Inc-Videos-Logo.png" alt="Profound Ideation Inc." className="ProfoundLogo" />
+        </div>  
+        <div className="search">
+          
+          <input onChange={ event => this.onInputChange(event.target.value) } />
+        </div>
       </div>
     );
   }
